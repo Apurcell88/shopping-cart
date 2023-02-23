ac0dc52a6e2194f345238841b229a4256bf9f9e9
Adam Purcell <apurcell88@gmail.com>
2023-02-23 15:41:50 -0600

    Built out the landing page and applied styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Link } from 'react-router-dom';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       {<Nav />}
-      <h1>Hello from App</h1>
+      <article className='landing-container'>
+        <h1 className='landing-text'>The World's Kitchen. Straight To Your Door.</h1>
+        <Link to='./shop' style={{textDecoration: 'none'}}>
+          <button className='shop-btn'>Shop Now</button>
+        </Link>
+      </article>
     </div>
   );
 }
