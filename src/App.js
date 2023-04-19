e02a4f4206fafd5acbd4e7a20087b096008225b1
Adam Purcell <apurcell88@gmail.com>
2023-04-19 16:00:40 -0500

    Added some style to App component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Nav';
+// import landingImg from './computer-landing-page.jpg';
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello from App</h1>
+      <div className="landing-container">
+        <h1>Nvidia RTX 4070 Ti</h1>
+        <h2>Available January 2023, preorder now.</h2>
+        <button>Shop Now</button>
+      </div>
     </div>
   );
 }
